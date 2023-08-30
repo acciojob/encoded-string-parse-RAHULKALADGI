e37715f1a9ecce96d28e692dfe825623af99d827
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-30 16:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const parseCode = (str) => {
   // your code here
+	  let a = {
+  }
+  let arr = [];
+  let ct = 0;
+  let f = "";
+  for(let i = 0;i<str.length;i++) {
+    if(str.charAt(i)!=='0') {
+      f+= str.charAt(i);
+    }
+    else {
+      if(f.length>0) {
+        arr.push(f);
+      }
+      ct = i;
+      f = "";
+    }
+  }
+  a.firstName = arr[0];
+  a.lastName = arr[1];
+  a.id = str.slice(ct+1,str.length); 
+  return a;
 };
 
 // Do not change the code below
